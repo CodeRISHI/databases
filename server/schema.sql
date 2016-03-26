d194244c8cd18677e84f5b143b68bdacf80de073
CodeRISHI <arunv83@gmail.com>
2016-03-25 17:30:19 -0700

    Created database and multi-table schema.

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,21 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  /*The first table is for the 'Users' - will keep track of 'user-id' and 'Username.'*/
+  userid int PRIMARY KEY,
+  username varchar(25) NOT NULL
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  /*The second table is for 'Messages' - will store 'message-id', 'message', and 'user-id_Users' which is a foreign key.*/
+
+  messageid int PRIMARY KEY,
+  message varchar(100) NOT NULL,
+  userref int NOT NULL,
+  FOREIGN KEY (userref) REFERENCES users(userid)
+
 );
 
 /* Create other tables and define schemas for them here! */
